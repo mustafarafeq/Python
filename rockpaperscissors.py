35f9d413aff91dba8a4b1a07224466cad6b4c0ce
Mustafa Rafeq <mustafarafeq@mustafas-laptop.mynetworksettings.com>
2025-10-13 22:30:45 -0400

    rockpaperscissors

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -1,6 +1,10 @@
 # We want the game to our computer play against the user.
-computer_choice = "scissors"
+import random
+
+computer_choice = random.choice(['rock', 'paper', 'scissors'])
 user_choice = input('Do you want rock, paper, or scissors? ')
+
+print('computer choice:', computer_choice)
 
 if computer_choice == user_choice:
         print('TIE!') # so far we have an 'if' statement that says, if the computer choice variable is equal to the user_choice variable from their input, then print "Tie!".
